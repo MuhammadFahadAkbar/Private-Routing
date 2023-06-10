112e3a010d4e0eb78ae0a572e1fe25c0dc29984d
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-06-10 15:09:27 +0500

    Added isLoggedIn to session storage

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Contact from "./Pages/Contact";
 import Register from "./Pages/Register";
-import { createContext, useState } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 interface AuthContextProps {
   isLoggedIn: boolean;
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
@@ -16,14 +16,25 @@ export const AuthContext = createContext<AuthContextProps>({
 });
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    const storedValue = sessionStorage.getItem("isLoggedIn");
+    return storedValue ? JSON.parse(storedValue) : false;
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem("isLoggedIn", isLoggedIn.toString());
+  }, [isLoggedIn]);
+
+  const HomeRoute = isLoggedIn ? <Home /> : <Navigate to="/login" />;
+  const LoginRoute = !isLoggedIn ? <Login /> : <Navigate to="/" />;
+
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" Component={isLoggedIn ? Home : Login} />
+          <Route path="/" element={HomeRoute} />;
           <Route path="/contact" Component={Contact} />
-          <Route path="/login" Component={Login} />
+          <Route path="/login" element={LoginRoute} />
           <Route path="/register" Component={Register} />
         </Routes>
       </BrowserRouter>
