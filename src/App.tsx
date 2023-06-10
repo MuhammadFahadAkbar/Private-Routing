46cacf5833f964653702b1e0af614d3440cf1904
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-06-10 14:18:17 +0500

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Contact from "./Pages/Contact";
+import Register from "./Pages/Register";
+
 const App = () => {
   return (
-    <h1>Private Routing with Typescript</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/contact" Component={Contact} />
+        <Route path="/login" Component={Login} />
+        <Route path="/register" Component={Register} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
