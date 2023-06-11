9450b3b241e1a0110d27b03b07f7fa7cfdf6d368
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-06-11 11:54:33 +0500

    Handle login form using formik

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,22 +1,48 @@
 import { useContext } from "react";
 import { AuthContext } from "../../App";
-import { Link, redirect } from "react-router-dom";
+import { redirect } from "react-router-dom";
+import { useFormik } from "formik";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
   const { setIsLoggedIn } = useContext(AuthContext);
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-    redirect("/");
-  };
+
+  const { values, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues: initialValues,
+    onSubmit: () => {
+      setIsLoggedIn(true);
+      console.log("Values", values);
+      redirect("/");
+    },
+  });
+  
   return (
     <>
       <h1>Log In</h1>
-      <form onSubmit={handleLogin}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={values.email}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
         <br />
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={values.password}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
         <br />
         <button type="submit">Submit</button>
       </form>
