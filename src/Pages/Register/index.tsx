3a83cd7e6805af8f54906770bdb83779dd87edd4
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-06-11 12:18:08 +0500

    Added validations for register form using Yup

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -1,4 +1,5 @@
 import { useFormik } from "formik";
+import { RegsiterSchema } from "../../schemas/RegisterSchema";
 
 const initialValues = {
   name: "",
@@ -8,12 +9,15 @@ const initialValues = {
 };
 
 const Register = () => {
-  const { values, handleBlur, handleChange, handleSubmit } = useFormik({
-    initialValues: initialValues,
-    onSubmit: () => {
-      console.log("Values", values);
-    },
-  });
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema: RegsiterSchema,
+      onSubmit: (values, action) => {
+        console.log("Values", values);
+        action.resetForm();
+      },
+    });
   return (
     <>
       <h1>Register</h1>
@@ -27,6 +31,7 @@ const Register = () => {
           onBlur={handleBlur}
           onChange={handleChange}
         />
+        {errors.name && touched.name && <p>{errors.name}</p>}
         <br />
         <label htmlFor="email">Email:</label>
         <input
@@ -37,6 +42,7 @@ const Register = () => {
           onBlur={handleBlur}
           onChange={handleChange}
         />
+        {errors.email && touched.email && <p>{errors.email}</p>}
         <br />
         <label htmlFor="password">Password:</label>
         <input
@@ -47,6 +53,7 @@ const Register = () => {
           onBlur={handleBlur}
           onChange={handleChange}
         />
+        {errors.password && touched.password && <p>{errors.password}</p>}
         <br />
         <label htmlFor="confirm_password">Confirm Password:</label>
         <input
@@ -57,6 +64,9 @@ const Register = () => {
           onBlur={handleBlur}
           onChange={handleChange}
         />
+        {errors.confirm_password && touched.confirm_password && (
+          <p>{errors.confirm_password}</p>
+        )}
         <br />
         <button type="submit">Register</button>
       </form>
