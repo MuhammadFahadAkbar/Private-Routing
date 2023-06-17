68acb87c30d52a7594effb1a15cb65d77fba0a43
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-06-17 12:04:55 +0500

    Removed extra code from files

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,21 +1,10 @@
-// import { useContext } from "react";
-// import { AuthContext } from "../../App";
-// import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 
 const Home = () => {
-  // const { setIsLoggedIn } = useContext(AuthContext);
-  // const handleLogin = () => {
-  //   setIsLoggedIn(false);
-  // };
-
   return (
-    <>
-      <Typography variant="h5" component="h5">Home</Typography>
-      {/* <Link to="/" onClick={handleLogin}>
-        Log Out
-      </Link> */}
-    </>
+    <Typography variant="h5" component="h5">
+      Home
+    </Typography>
   );
 };
 
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,57 +1,10 @@
-// import { useContext } from "react";
-// import { AuthContext } from "../../App";
-// import { redirect } from "react-router-dom";
-// import { useFormik } from "formik";
-// import { LoginSchema } from "../../schemas/LoginSchema";
 import { Typography } from "@mui/material";
-// const initialValues = {
-//   email: "",
-//   password: "",
-// };
 
 const Login = () => {
-  // const { setIsLoggedIn } = useContext(AuthContext);
-
-  // const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
-  //   useFormik({
-  //     initialValues,
-  //     validationSchema: LoginSchema,
-  //     onSubmit: () => {
-  //       setIsLoggedIn(true);
-  //       console.log("Values", values);
-  //       redirect("/");
-  //     },
-  //   });
-
   return (
-    <>
-      <Typography variant="h5" component="h5">Log In</Typography>
-      {/* <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={values.email}
-          onBlur={handleBlur}
-          onChange={handleChange}
-        />
-        {errors.email && touched.email && <p>{errors.email}</p>}
-        <br />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={values.password}
-          onBlur={handleBlur}
-          onChange={handleChange}
-        />
-        {errors.password && touched.password && <p>{errors.password}</p>}
-        <br />
-        <button type="submit">Login</button>
-      </form> */}
-    </>
+    <Typography variant="h5" component="h5">
+      Log In
+    </Typography>
   );
 };
 
