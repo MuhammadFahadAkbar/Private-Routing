9464634ed3f336077d317163da5a49c51f50f8dc
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-06-17 11:56:39 +0500

    Added MUI Styling

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,7 @@
 // import { useContext } from "react";
 // import { AuthContext } from "../../App";
 // import { Link } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
   // const { setIsLoggedIn } = useContext(AuthContext);
@@ -9,22 +9,12 @@ const Home = () => {
   //   setIsLoggedIn(false);
   // };
 
-  const { logout } = useAuth0();
-
   return (
     <>
-      <h1>Home</h1>
+      <Typography variant="h5" component="h5">Home</Typography>
       {/* <Link to="/" onClick={handleLogin}>
         Log Out
       </Link> */}
-
-      <button
-        onClick={() =>
-          logout({ logoutParams: { returnTo: window.location.origin } })
-        }
-      >
-        Log Out
-      </button>
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { Container } from "@mui/material";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-     <Auth0Provider
-        domain="dev-vv56j5skcswxbfmp.us.auth0.com"
-        clientId="8JjPHpSxGLglHU6NJu0yhWYsrpVxavzV"
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-        }}
-      >
-    <App />
+    <Auth0Provider
+      domain="dev-vv56j5skcswxbfmp.us.auth0.com"
+      clientId="8JjPHpSxGLglHU6NJu0yhWYsrpVxavzV"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <Container maxWidth="lg">
+        <App />
+      </Container>
     </Auth0Provider>
   </React.StrictMode>
 );
